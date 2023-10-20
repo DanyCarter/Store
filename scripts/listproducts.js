2e59ff50a6c8f03ac02c0e70e2a37a1718307a89
Alvaro <alvarodromantligero@alumnos.tuniversformacion.es>
2023-10-20 12:08:43 +0200

    creando los js

---
+++ scripts/listproducts.js
@@ -0,0 +1,37 @@
+import { inventario } from "./products.js";
+
+export const listproducts = () => {
+
+    const table = document.getElementById("inventario-tabla")
+
+    inventario.forEach(item => {
+
+        const row = table.insertCell()
+
+        const cell1 = row.insertCell(0)
+        const cell2 = row.insertCell(1)
+        const cell3 = row.insertCell(2)
+        const cell4 = row.insertCell(3)
+
+        cell1.innerHTML = item.nombre
+        cell2.innerHTML = item.cantidad
+        cell3.innerHTML = item.precio
+
+        const deleteButton = document.createElement("button")
+        deleteButton.innerText = "BORRAR"
+        deleteButton.addEventListener("click", () => {
+
+        })
+
+        cell4.appendChild(deleteButton)
+
+        const editButton = document.createElement("button")
+        editButton.innerText = "EDITAR"
+        editButton.addEventListener("click", () => {
+
+        })
+
+        cell4.appendChild(editButton)
+        
+    });
+}
---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+import {listproducts} from "./listproducts.js"
+
+/**
+ * Autor: Alvaro Dromant Ligero(Estudiante)
+ * GitHub: 
+ */
+
+// Listamos los productos
+document.addEventListener("DOMContentLoaded", listproducts)
